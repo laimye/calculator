d50483721fd71bc3c9845627886a74af166ad19a
Elena <elena.korotich@gmail.com>
2016-12-02 16:42:50 -0800

    Added markup styling and little js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('js loaded')
+/*--- variables ---*/
+
+var input, firstNum, op, result;
+var displayEl = document.getElementById('display');
+
+/*--- event listeners ---*/
+
+
+
+/*--- functions ---*/
+
+function initialize() {
+	input = '';
+	firstNum = op = results = null;
+	updateDisplay();
+}
+
+function updateDisplay() {
+	var text = result || input || '0.';
+	displayEl.textContent = text;
+}
